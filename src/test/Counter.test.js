1f6b2d3b16131a54d398ce45cd7572dd2fed8510
정내혁 <cnh0301@gmail.com>
2024-05-11 00:50:27 +0900

    test: test case 추가

--- src/test/Counter.test.js
+++ src/test/Counter.test.js
@@ -5,13 +5,13 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import Counter from "../Counter";
 
 describe("Counter", () => {
-  test("Counter의 처음 초기 값은 0이다.", () => {
+  test("의 처음 초기 값은 0이다.", () => {
     render(<Counter />);
     const counter = screen.getByText(/0/i);
     expect(counter).toBeInTheDocument();
   });
 
-  test("Counter의 - 버튼을 클릭하면 1 감소한다.", () => {
+  test("의 - 버튼을 클릭하면 1 감소한다.", () => {
     render(<Counter />);
     const minus = screen.getByText(/-/i);
     fireEvent.click(minus);
@@ -19,11 +19,31 @@ describe("Counter", () => {
     expect(counter).toBeInTheDocument();
   });
 
-  test("Counter의 + 버튼을 클릭하면 1 증가한다.", () => {
+  test("의 + 버튼을 클릭하면 1 증가한다.", () => {
     render(<Counter />);
     const plus = screen.getByText(/\+/i);
     fireEvent.click(plus);
     const counter = screen.getByText(/1/i);
+    expect(counter).toBeInTheDocument();
+  });
+
+  test("의 최대값은 10이다.", () => {
+    render(<Counter />);
+    const plus = screen.getByText(/\+/i);
+    for (let i = 0; i < 11; i++) {
+      fireEvent.click(plus);
+    }
+    const counter = screen.getByText(/10/i);
+    expect(counter).toBeInTheDocument();
+  });
+
+  test("의 최소값은 -10이다.", () => {
+    render(<Counter />);
+    const minus = screen.getByText(/-/i);
+    for (let i = 0; i < 11; i++) {
+      fireEvent.click(minus);
+    }
+    const counter = screen.getByText(/-10/i);
     expect(counter).toBeInTheDocument();
   });
 });
--- src/test/ShowBox.test.js
+++ src/test/ShowBox.test.js
@@ -2,11 +2,14 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import ShowBox from "../ShowBox";
 
 describe("ShowBox", () => {
-  test("의 처음 초기 값은 빈 문자열이다.", () => {
+  test("의 Input에 값이 입력되어 있지 않으면, '값을 입력해주세요'가 출력된다.", () => {
     render(<ShowBox />);
     const pElement = screen.getByRole("paragraph");
     expect(pElement).toBeInTheDocument();
-    expect(pElement).toHaveTextContent("");
+    expect(pElement).toHaveTextContent("값을 입력해주세요");
+
+    const inputElement = screen.getByRole("textbox");
+    expect(inputElement).toHaveValue("");
   });
 
   test("의 Input에 값을 입력하면 p 태그에 텍스트가 출력된다.", () => {
