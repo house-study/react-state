77ca2f214ccef2e673c8180d1022853aafb4683f
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2024-05-13 20:44:50 +0900

    feat: input 입력 구현

--- src/Input/index.js
+++ src/Input/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Input = () => {
-  return <input />;
+
+
+const Input = ({value, onChange}) => {
+  return(
+    <div>
+      <p> Type anything! </p>
+      <input value={value} onChange={onChange} />;
+   </div>
+  )
 };
 
 export default Input;
