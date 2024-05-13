5ad2acc5fc211f17951dc6534ccfecf9a330cc14
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2024-05-13 19:33:22 +0900

    feat: 카운터 생성 및 예외처리 진행

--- src/Counter/index.js
+++ src/Counter/index.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 // Counter의 시작은 0부터 시작합니다.
 const Counter = () => {
+  const [count, setCount] = useState(0);
+  
+  const clickPlus = () =>{
+    if(count <10){
+      setCount(count+1);
+    }
+    else{
+      alert("10이 최대입니다!");
+    }
+  }
+
+  const clickMinus = () =>{
+    if(count>-10 ){
+      setCount(count-1);
+    }
+    else{
+      alert("-10이 최소입니다!");
+    }
+  }
+
   return (
     <div className="counter--container">
-      <button>-</button>
-      <p>{/** 여기에 카운트가 나타납니다. 주석 삭제 후 작성해주세요 */}</p>
-      <button>+</button>
+      <button onClick={clickMinus}>-</button>
+      <p>{
+          count
+        }{}
+        </p>
+      <button onClick={clickPlus}>+</button>
     </div>
   );
 };
