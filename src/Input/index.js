6c2a93ef848fc5aa5d5a90aa85b0df8224f44c47
guddnboy <hung3018@naver.com>
2024-05-16 11:30:12 +0900

    '값을 입력하세요' 추가

--- src/Input/index.js
+++ src/Input/index.js
@@ -2,16 +2,16 @@ import React, {useState} from "react";
 
 const Input = () => {
   const [text, setTxt] = useState('');
+  const displayNull = '값을 입력하세요';
 
   const updateText = (e) => {
       setTxt(e.target.value);
   }
-
   return (
   <div className="show-box--container">
     <input onChange={updateText} value={text} />
     <p className="show-box--text">
-    {text}
+    {text === '' ? displayNull : text}
     </p>
     </div>
     );
