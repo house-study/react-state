ef72e10eceba92042395bc0a83a54d86f111edeb
guddnboy <hung3018@naver.com>
2024-05-18 15:19:19 +0900

    props 사용

--- src/Counter/index.js
+++ src/Counter/index.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 // Counter의 시작은 0부터 시작합니다.
 const Counter = () => {
+  const [Count, setCount] = useState(0);
+  const CountIncrease = () =>{
+    if(Count <10){
+      setCount(Count  + 1);
+    }
+  }
+
+  const CountDecrease = () =>{
+    if(Count > -10){
+      setCount(Count - 1);
+    }
+  }
+
   return (
     <div className="counter--container">
-      <button>-</button>
-      <p>{/** 여기에 카운트가 나타납니다. 주석 삭제 후 작성해주세요 */}</p>
-      <button>+</button>
+      <button onClick={CountDecrease}>-</button>
+      <p>{Count}</p>
+      <button onClick={CountIncrease}>+</button>
     </div>
   );
 };
--- src/Input/index.js
+++ src/Input/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-const Input = () => {
-  return <input />;
+
+const Input = ({value, onChange}) => {
+  
+  return <input value={value} onChange={onChange} />;
 };
 
 export default Input;
